9172afdeb6ef45c67d9d198bc62909ad0091ca8f
lucascr91 <lucas.ecomg@gmail.com>
2020-12-26 22:28:38 -0300

    Update README

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function caesar(message, key, mode){
     let SYMBOLS='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
-    let translated
-    let translatedIndex
+    let translated=''
+    let translatedIndex=0
 
     for (symbol of message){
         if (SYMBOLS.includes(symbol)){
@@ -16,7 +16,7 @@ function caesar(message, key, mode){
                 translatedIndex = translatedIndex-SYMBOLS.length
             }
             else if (translatedIndex<0){
-                translatedIndex = translatedIndex-SYMBOLS.length
+                translatedIndex = translatedIndex+SYMBOLS.length
             }
             translated=translated+ SYMBOLS[translatedIndex]}
         else {
@@ -30,4 +30,44 @@ function caesar(message, key, mode){
 function encrypt() {
     return caesar(message=document.getElementById("user-text").value,
     key=Number(document.getElementById("key").value), mode="encrypt")
+}
+
+function decrypt() {
+    return caesar(message=document.getElementById("user-text").value,
+    key=Number(document.getElementById("key").value), mode="decrypt")
+}
+
+function copy(){
+    encText=document.getElementById("enc-text").value
+    return copyTextToClipboard(encText)
+}
+
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+  textArea.style.padding = 0;
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+  textArea.style.background = 'transparent';
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+    window.prompt("Copie para área de transferência: Ctrl+C e tecle Enter", text);
+  }
+
+  document.body.removeChild(textArea);
 }
